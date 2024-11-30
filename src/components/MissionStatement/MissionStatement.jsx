6842c81117a9d75c4a0344edece6a67ddba053c3
Gharsa Amin <ghars0718@gmail.com>
2024-11-30 14:35:36 -0500

    made the MissionStatement responsive across all three different screens

--- src/components/MissionStatement/MissionStatement.jsx
+++ src/components/MissionStatement/MissionStatement.jsx
@@ -6,7 +6,8 @@ export default function Header() {
 			<h4 className="mission-section__title">Our mission:</h4>
 			<p className="mission-section__paragraph">
 				Provide photographers a space to share photos of the neighborhoods they
-				cherish, <span>expressed in their unique style</span>.
+				cherish,
+				<span className="modifier"> expressed in their unique style.</span>
 			</p>
 		</section>
 	);

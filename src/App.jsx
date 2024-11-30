92468cbdd5d2fcb33087486aa92c9b836f44581c
Gharsa Amin <ghars0718@gmail.com>
2024-11-30 02:36:22 -0500

    made sure onClick works properly when state is active, and props are used correctly

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,8 @@ export default function App() {
 	const filterButtonClass = isFilterOpen ? "filter-cta active" : "filter-cta";
 
 	return (
-		<>
-			<div className="Nav-bar">
+		<main>
+			<div className="header">
 				<SnapsLogo />
 				<button onClick={toggleFilterDrawer} className={filterButtonClass}>
 					<img src={image} alt="Filter icon" />
@@ -43,60 +43,12 @@ export default function App() {
 
 				<Header />
 
-				{/* Photo gallery with isFilterOpen passed to control layout */}
+				{/* Photo gallery with isFilterOpen passed to determine the layout */}
 				<article className="photo-gallery-container">
 					<PhotosGallery selectedTag={selectedTag} />
 				</article>
 			</div>
 			<Footer />
-		</>
+		</main>
 	);
 }
-
-// export default function App() {
-// 	const [isFilterOpen, setIsFilterOpen] = useState(false); // Whether the filter drawer is open
-// 	const [selectedTag, setSelectedTag] = useState(null); // The currently selected tag
-
-// 	const handleTagClicked = (tag) => {
-// 		// If a tag is selected, and the same tag is clicked again, deselect it
-// 		if (selectedTag === tag) {
-// 			setSelectedTag(null); // Deselect the tag
-// 		} else {
-// 			setSelectedTag(tag); // Select the new tag
-// 		}
-// 	};
-
-// 	const toggleFilterDrawer = () => {
-// 		setIsFilterOpen((prev) => !prev); // Toggle filter drawer open/close
-// 	};
-// 	//change Nav-bar to header.
-// 	return (
-// 		<>
-// 			<div className="Nav-bar">
-// 				<SnapsLogo />
-// 				<button onClick={toggleFilterDrawer} className="filter-cta">
-// 					<img
-// 						src={image}
-// 						alt="Filter icon"
-// 						className="filter-icon filter-icon-active"
-// 					/>
-// 					{isFilterOpen ? "Filters" : "Filters"}
-// 				</button>
-// 			</div>
-// 			{/* Filter drawer */}
-// 			{isFilterOpen && (
-// 				<div className="filter-drawer">
-// 					<FilterPanel
-// 						tags={tags}
-// 						handleTagClicked={handleTagClicked}
-// 						selectedTag={selectedTag} // Pass selectedTag to highlight the active tag
-// 					/>
-// 				</div>
-// 			)}
-// 			<Header />
-// 			<PhotosGallery selectedTag={selectedTag} />{" "}
-// 			{/* Pass selectedTag to filter photos */}
-// 			<Footer />
-// 		</>
-// 	);
-// }

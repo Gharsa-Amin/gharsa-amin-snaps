ea640821404b26c6287669c2f6da29f7dc2496c6
Gharsa Amin <ghars0718@gmail.com>
2024-12-05 23:40:13 -0500

    passed id as props to the AboutPage to be globally available to all the components

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import PhotoDetails from "../../components/PhotoDetails/PhotoDetails";
 import "./AboutPage.scss";
-// import Form from "../../components/Form/Form";
 import GetComment from "../../components/GetComment/GetComment";
 import PostComment from "../../components/PostComment/PostComment";
 
@@ -11,10 +10,8 @@ export default function AboutPage() {
 
 	return (
 		<div className="about-page">
-			{/* Pass the ID as a prop to PhotoDetails */}
 			<PhotoDetails photoId={id} />
 			<PostComment photoId={id} />
-			{/* <Form /> */}
 			<GetComment photoId={id} />
 		</div>
 	);

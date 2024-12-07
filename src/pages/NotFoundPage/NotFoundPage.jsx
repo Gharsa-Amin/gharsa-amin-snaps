f6c206b88093b9feace7edea3c29af9bdc70bb49
Gharsa Amin <ghars0718@gmail.com>
2024-12-07 13:33:45 -0500

    made the NotFoundPage link as part of the dynamic UI

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import "./NotFoundPage.scss";
 
 export default function NotFoundPage() {
 	return (
 		<>
 			<h2>Whooopsies! Page Not found⚠️</h2>
 			<Link to="/">
-				<p>Click here to get back to the HomePage</p>
+				<p className="redirectional-link">
+					Click here to get back to the HomePage
+				</p>
 			</Link>
 		</>
 	);

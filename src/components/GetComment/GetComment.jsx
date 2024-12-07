d2ac28f883e5a22699bade04ff8a124507dc991f
Gharsa Amin <ghars0718@gmail.com>
2024-12-06 22:24:09 -0500

    created responsive layouts for the comments section

--- src/components/GetComment/GetComment.jsx
+++ src/components/GetComment/GetComment.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./GetComment.scss";
 
 export default function Comments({ comments }) {
 	// const [comments, setComments] = useState([]);
@@ -26,10 +27,12 @@ export default function Comments({ comments }) {
 		<section className="comment-section">
 			{comments.map((comment) => (
 				<div key={comment.id} className="comment-item">
-					<p className="comment-item__author">{comment.name}</p>
-					<p className="comment-item__time">
-						{new Date(comment.timestamp).toLocaleDateString()}
-					</p>
+					<div className="comment-item__wrapper">
+						<p className="comment-item__author">{comment.name}</p>
+						<p className="comment-item__time">
+							{new Date(comment.timestamp).toLocaleDateString()}
+						</p>
+					</div>
 					<p className="comment-item__comment">{comment.comment}</p>
 				</div>
 			))}

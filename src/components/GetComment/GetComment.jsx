dcfc465b4885466b38fd8f0b55be963c820914fe
Gharsa Amin <ghars0718@gmail.com>
2024-12-07 15:27:52 -0500

    final QC done, ready to be submitted

--- src/components/GetComment/GetComment.jsx
+++ src/components/GetComment/GetComment.jsx
@@ -6,7 +6,7 @@ export default function Comments({ comments }) {
 	const commentCount = comments.length;
 	return (
 		<section className="comment-section">
-			<p>{commentCount} Comment</p>
+			<p className="comment-section__count">{commentCount} Comment</p>
 			{comments.map((comment) => (
 				<div key={comment.id} className="comment-item">
 					<div className="comment-item__wrapper">
